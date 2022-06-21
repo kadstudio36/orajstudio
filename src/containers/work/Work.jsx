48c6f4d16ba0104073ca82638b2620a4e517b94d
orajstudio <orajstudio@gmail.com>
2022-06-21 01:04:12 -0500

    fufu212

--- src/containers/work/Work.jsx
+++ src/containers/work/Work.jsx
@@ -1,9 +1,128 @@
 import React from 'react'
 import './work.css';
+import arrowdiagoblack from '../../assets/arrowdiagoblack.svg'
+
+import toastcoinimg from '../../assets/toastcoin-prev-full.jpg'
+import toastcoinlogo from '../../assets/toastcoin-logo.svg'
+
+import muspaceimg from '../../assets/muspace-prev-full.jpg'
+import muspacelogo from '../../assets/muspace-logo.svg'
+
+const Toastcoin = () => (
+<>
+                  <div className="oraj__work-content_toastcoin">
+                    <div className="oraj__work-content_toastcoinbg">
+                    <div className="oraj__work-content_toastcoinleft">
+                        <div className="oraj__work-content_toastcointextbox">
+                          <div className="oraj__work-content_toastcointags">
+                            <div className="oraj__work-content_toastcointagsbox"><h1>WEBSITE</h1></div>
+                            <div className="oraj__work-content_toastcointagsbox"><h1>BRANDING</h1></div>
+                            <div className="oraj__work-content_toastcointagsbox"><h1>MOTION</h1></div>
+                            <div className="oraj__work-content_toastcointagsbox"><h1>UI/UX</h1></div>
+                            <div className="oraj__work-content_toastcointagsbox"><h1>3D</h1></div>
+                          </div>
+                          <div className="oraj__work-content_toastcoinlogo">
+                            <img src={toastcoinlogo} alt={toastcoinlogo} /> <h1>Toastcoin</h1>
+                          </div>
+                          <div className="oraj__work-content_toastcoindescription">
+                              <h1>Building a promotional/informational website<br></br>for a cryptocurrency project.</h1>
+                          </div>
+                          <div className="oraj__work-content_toastcoinbutton">
+                          <button type="button"><h1>See the full Case Study</h1><img src={arrowdiagoblack} alt="arrowdiagoblack" /></button>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="oraj__work-content_toastcoinright">
+                        <div className="oraj__work-content_toastcoinimg">
+                        <img src={toastcoinimg} alt={toastcoinimg} />
+                      </div>
+                      </div>
+                    </div>
+                  </div>
+</>
+)
+
+const Muspace = () => (
+  <>
+                    <div className="oraj__work-content_toastcoin">
+                      <div className="oraj__work-content_muspacebg">
+                      <div className="oraj__work-content_toastcoinleft">
+                          <div className="oraj__work-content_toastcointextbox">
+                            <div className="oraj__work-content_toastcointags">
+                              <div className="oraj__work-content_toastcointagsbox"><h1>WEBSITE</h1></div>
+                              <div className="oraj__work-content_toastcointagsbox"><h1>DESKTOP APP</h1></div>
+                              <div className="oraj__work-content_toastcointagsbox"><h1>BRANDING</h1></div>
+                              <div className="oraj__work-content_toastcointagsbox"><h1>MOTION</h1></div>
+                              <div className="oraj__work-content_toastcointagsbox"><h1>UI/UX</h1></div>
+                            </div>
+                            <div className="oraj__work-content_toastcoinlogo">
+                              <img src={muspacelogo} alt={muspacelogo} /> <h1>Mu.space</h1>
+                            </div>
+                            <div className="oraj__work-content_toastcoindescription">
+                                <h1>Building a file-sharing platform and promotional<br></br> website focusing mainly on creating a space to<br></br> organize and ideate for musicians.</h1>
+                            </div>
+                            <div className="oraj__work-content_toastcoinbutton">
+                            <button type="button"><h1>See the full Case Study</h1><img src={arrowdiagoblack} alt="arrowdiagoblack" /></button>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="oraj__work-content_toastcoinright">
+                          <div className="oraj__work-content_toastcoinimg">
+                          <img src={muspaceimg} alt={muspaceimg} />
+                        </div>
+                        </div>
+                      </div>
+                    </div>
+  </>
+  )
+
+
+
+
 const Work = () => {
   return (
     <div>
-        Work
+          <div className="oraj__work section__padding" id="work">
+            <div className="oraj__work-content">
+
+              <div className="oraj__work-content_uppertext">
+                <h1>Latest work</h1><h2>See our featured projects and case studies.</h2>
+
+              </div>
+                <div className="oraj__work-content_container">
+                  <Toastcoin /> <Muspace />
+                  <div className="oraj__work-content_toastcoin">
+                    <div className="oraj__work-content_toastcoinbg">
+                    <div className="oraj__work-content_toastcoinleft">
+                        <div className="oraj__work-content_toastcointextbox">
+                          <div className="oraj__work-content_toastcointags">
+                            <div className="oraj__work-content_toastcointagsbox"><h1>WEBSITE</h1></div>
+                            <div className="oraj__work-content_toastcointagsbox"><h1>BRANDING</h1></div>
+                            <div className="oraj__work-content_toastcointagsbox"><h1>MOTION</h1></div>
+                            <div className="oraj__work-content_toastcointagsbox"><h1>UI/UX</h1></div>
+                            <div className="oraj__work-content_toastcointagsbox"><h1>3D</h1></div>
+                          </div>
+                          <div className="oraj__work-content_toastcoinlogo">
+                            <img src={toastcoinlogo} alt={toastcoinlogo} /> <h1>Toastcoin</h1>
+                          </div>
+                          <div className="oraj__work-content_toastcoindescription">
+                              <h1>Building a promotional/informational website<br></br>for a cryptocurrency project.</h1>
+                          </div>
+                          <div className="oraj__work-content_toastcoinbutton">
+                          <button type="button"><h1>See the full Case Study</h1><img src={arrowdiagoblack} alt="arrowdiagoblack" /></button>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="oraj__work-content_toastcoinright">
+                        <div className="oraj__work-content_toastcoinimg">
+                        <img src={toastcoinimg} alt={toastcoinimg} />
+                      </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
     </div>
   )
 }
