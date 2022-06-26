56c1f83232ea1fe2af1c71640b7b860d71a9bbf4
orajstudio <orajstudio@gmail.com>
2022-06-26 18:10:53 -0400

    adadads

--- src/containers/form/Form.jsx
+++ src/containers/form/Form.jsx
@@ -3,7 +3,21 @@ import './form.css';
 const Form = () => {
   return (
     <div>
-        Form
+            <div>
+          <div className="oraj__form section__padding" id="form">
+            <div className="oraj__form-content">
+
+              <div className="oraj__form-content_uppertext">
+                <h1>Have an idea? Let’s talk!</h1><h2>We offer free consultations. Tell us what you need below!</h2>
+
+              </div>
+                <div className="oraj__form-content_container">
+                 
+                  
+                </div>
+              </div>
+            </div>
+    </div>
     </div>
   )
 }
