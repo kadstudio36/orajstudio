090c79d64da59b5fd8766c229e15f35537d4d742
orajstudio <orajstudio@gmail.com>
2022-08-15 20:42:39 -0400

    faviicon

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
 import { About, Footer, Form, Header, Work, Workpage, Contact, Toastcoin, Muspace } from './containers';
 import { Homepage } from './containers';
 import { Navbar } from './components';
+import {Helmet} from "react-helmet";
 import './App.css';
 
 
@@ -15,6 +16,14 @@ import './App.css';
 const App = () => {
     return (
         <div className="App">
+
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>Orajstudio</title>
+                <link rel="canonical" href="http://orajstudio.com/" />
+                <meta name="description" content="Oraj Studio is a creative agency with market-leading design and development skills. We help you transform your brand and make the digital shift." />
+            </Helmet>
+
             <div className="gradient_bg"></div>
             <BrowserRouter>
                 <Navbar />
